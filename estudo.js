6211ee6b8a59c42bda634070de7cb426fa4fd416
danielgarcia1c <110104441+danielgarcia1c@users.noreply.github.com>
2022-10-19 10:01:19 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -42,3 +42,17 @@ function mat(){
 }
     document.write("Resultado " + r)
 }
+function soma(){
+    let n1 = document.getElementById("v1").value;
+    let n2 = document.getElementById("v2").value;
+    let n3 = document.getElementById("v3").value;
+    let r = (Number(n1) + Number(n2) + Number(n3));
+    document.getElementById("resultado").innerHTML = "Soma: " + r;
+}
+function media(){
+    let n1 = document.getElementById("v1").value;
+    let n2 = document.getElementById("v2").value;
+    let n3 = document.getElementById("v3").value;
+    let v = (Number(n1) + Number(n2) + Number(n3))/3;
+    document.getElementById("resultado").innerHTML = "Média: " + v;
+}
